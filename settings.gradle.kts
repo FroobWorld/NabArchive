e57dfd69588f74c9f531fa8c594506df8fdac120
froobynooby <froobynooby@froobworld.com>
2022-02-28 17:54:10 +0930

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://papermc.io/repo/repository/maven-public/")
+    }
+}
+
+rootProject.name = "nabarchive"
+
+include("nabarchive-api", "nabarchive-server")
